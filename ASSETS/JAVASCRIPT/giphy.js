a720c0afce8a7d4b7c597c03edecb1249b38d101
Polaris <tristynkcline@gmail.com>
2019-10-01 17:29:52 -0500

    Update4 JAVASCRIPT HTML

--- ASSETS/JAVASCRIPT/giphy.js
+++ ASSETS/JAVASCRIPT/giphy.js
@@ -1,7 +1,7 @@
 $("button").on("click", function () {
-    var dinosaur = $(this).attr("data-dinosaur");
+    var dinosaur = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        dinosaur + "&api_key=0FEUTWqxcNww2JBF5jHJ8ehQQYva0Z9a";
+        dinosaur + "&api_key=0FEUTWqxcNww2JBF5jHJ8ehQQYva0Z9a&limit=10";
 
 
     $.ajax({
@@ -35,3 +35,6 @@ $("button").on("click", function () {
     });
 
 });
+
+
+  
