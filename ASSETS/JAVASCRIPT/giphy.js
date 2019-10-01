f20eaa5c3c407f9a0f9051f34751bd00c1b379bd
Polaris <tristynkcline@gmail.com>
2019-10-01 08:56:05 -0500

    Update1 CSS

--- ASSETS/JAVASCRIPT/giphy.js
+++ ASSETS/JAVASCRIPT/giphy.js
@@ -34,3 +34,19 @@ $("button").on("click", function() {
 
     });
   });
+
+
+  $(".gif").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
